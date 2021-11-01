4dfb9a3ed359743e1f21b898f0bd737d5b7cee44
Luis Escobar <lae311@stern.nyu.edu>
2021-11-01 17:00:49 -0400

    commit tree 92324a9a7df87ec700fe29015cf450feba73b79f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  
+  const [inCart, setCart] = useState(false)
+  
+  function handleClick () {
+    setCart((inCart) => !inCart)
+  }
+
+  const cartClass = inCart ? "in-cart" : ""
+
+  const cartButton = inCart ? "Remove From Cart" : "Add to Cart"
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{cartButton}</button>
     </li>
   );
 }
